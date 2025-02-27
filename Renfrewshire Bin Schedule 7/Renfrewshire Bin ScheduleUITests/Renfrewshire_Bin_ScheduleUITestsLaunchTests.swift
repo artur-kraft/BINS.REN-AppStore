3b673176b4d52c1d10bb2ce439e4fbed97bda966
Artur <artur.kraft@outlook.com>
2025-02-27 17:01:22 +0000

    2.2 initial commit

---
+++ Renfrewshire Bin Schedule 7/Renfrewshire Bin ScheduleUITests/Renfrewshire_Bin_ScheduleUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  Renfrewshire_Bin_ScheduleUITestsLaunchTests.swift
+//  Renfrewshire Bin ScheduleUITests
+//
+//  Created by Artur Kraft on 17/02/2025.
+//
+
+import XCTest
+
+final class Renfrewshire_Bin_ScheduleUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
